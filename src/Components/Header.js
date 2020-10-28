bb587bff0f47f3c6d3542469e1ebd57a7ff759c5
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2020-10-28 17:07:39 -0300

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SectionHeader } from '../Styles/Header'
+
+const Header = () => {
+  return (
+    <>
+      <SectionHeader>
+        Funcionando!
+      </SectionHeader>
+    </>
+  )
+}
+
+export default Header
+
