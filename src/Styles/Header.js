6031acc23218ec165e4a980ceda00bd583183337
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2020-10-29 12:24:21 -0300

    style(Header) add styles to components

--- src/Styles/Header.js
+++ src/Styles/Header.js
@@ -2,4 +2,31 @@ import styled from 'styled-components';
 
 export const SectionHeader = styled.div`
   background-color: #fff159;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ImageLogo = styled.img`
+  width: 44px;
+  height: 32px;
+`;
+
+export const InputSearch = styled.input`
+  border: none;
+  padding: 10px;
+  outline: none;
+`;
+
+export const ButtonSearch = styled.button`
+  border: none;
+  padding: 10px;
+  :focus {
+    outline: none;
+  }
+`;
+
+export const ImageSearch = styled.img`
+  width: 20px;
 `;
